207fd543cfbd46673a0590eff8b0d254a95068e4
Smit Shah <101961910+smit-shah-GG@users.noreply.github.com>
2023-11-17 20:53:19 +0530

    Removed redundancies

--- main.py
+++ main.py
@@ -1,89 +1,84 @@
-from io import StringIO
-from os import write
 import streamlit as st
 import pandas as pd
 import plotly.express as px
 import seaborn as sns
 
-st.set_page_config(page_title='DWM Mini Project', page_icon=':bar_chart:', layout="wide")
+st.set_page_config(
+    page_title="DWM Mini Project", page_icon=":bar_chart:", layout="wide"
+)
+
 
 # Define the first page
 def page1():
     uploaded_file = st.file_uploader("Choose a CSV file", type=["csv"])
 
     if uploaded_file is not None:
-        # To read file as bytes:
-        bytes_data = uploaded_file.getvalue()
-
-        # To convert to a string based IO:
-        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-
-        # To read file as string:
-        string_data = stringio.read()
         df = pd.read_csv(uploaded_file)
-        st.session_state['df'] = df
-        st.success('File uploaded successfully!')
+        st.session_state["df"] = df
+        st.success("File uploaded successfully!")
+
 
 def page2():
-    df = st.session_state.get('df')
+    df = st.session_state.get("df")
     col1, col2 = st.columns(2)
     with col1:
         # Create the dropdown menus
         cols = df.columns
-        x_col = st.selectbox('Select an X-axis column', cols)
-        y_col = st.selectbox('Select a Y-axis column', cols)
-        graph_type = st.selectbox('Select a graph type', ['scatter', 'line', 'bar'])
+        x_col = st.selectbox("Select an X-axis column", cols)
+        y_col = st.selectbox("Select a Y-axis column", cols)
+        graph_type = st.selectbox("Select a graph type", ["scatter", "line", "bar"])
     with col2:
-
         # Plot the graph
-        if graph_type == 'scatter':
+        if graph_type == "scatter":
             fig = px.scatter(df, x=x_col, y=y_col)
-        elif graph_type == 'line':
+        elif graph_type == "line":
             fig = px.line(df, x=x_col, y=y_col)
         else:
             fig = px.bar(df, x=x_col, y=y_col)
         st.plotly_chart(fig)
 
+
 def page3():
-    df = st.session_state.get('df')
+    df = st.session_state.get("df")
     cols = df.columns
 
     df2 = df[cols]
 
-    lowerplots = ['kde', 'line']
+    lowerplots = ["kde", "line"]
 
     lower_selected = st.selectbox("Select the lower plot", lowerplots)
     if st.button("Generate"):
         fig = sns.pairplot(df2)
-        if lower_selected == 'kde':
+        if lower_selected == "kde":
             fig.map_lower(sns.kdeplot)
-        else: 
+        else:
             fig.map_lower(sns.lineplot)
 
         st.pyplot(fig)
 
+
 def page4():
     st.write("Database Cleaning")
-    df2 = st.session_state.get('df')
+    df2 = st.session_state.get("df")
     cols = df2.columns
-    col5, col7, col8 = st.columns([1,1,1])
+    col5, col7, col8 = st.columns([1, 1, 1])
 
     with col5:
-        options = ['drop', 'fill', 'global_const']
+        options = ["drop", "fill", "global_const"]
 
-        option_selected = st.selectbox('Select an option', options)
-        column_selected = st.selectbox('Select column', cols)
+        option_selected = st.selectbox("Select an option", options)
+        column_selected = st.selectbox("Select column", cols)
 
-        if option_selected == 'drop':
+        if option_selected == "drop":
             df2 = df2.drop(column_selected, axis=1)
-        elif option_selected == 'fill':
+        elif option_selected == "fill":
             df2 = df2.fillna(inplace=True)
         else:
             val = st.text_input("Enter Global Const")
             df2 = df2.fillna(value=val)
 
     with col7:
-        df = st.session_state.get('df')
+        df = st.session_state.get("df")
         st.write("Old database:")
         st.write(df)
 
@@ -91,15 +86,17 @@ def page4():
         st.write("New database:")
         st.write(df2)
 
-# Create the app
-st.sidebar.title('Navigation')
-page = st.sidebar.radio('Go to', ['Upload CSV', 'Plot Data', 'Generated Plots', 'Database Manipulation'])
-if page == 'Upload CSV':
-    page1()
-elif page == 'Plot Data':
-    page2()
-elif page == 'Generated Plots':
-    page3()
-elif page == 'Database Manipulation':
-    page4()
 
+# Create the app
+st.sidebar.title("Navigation")
+page = st.sidebar.radio(
+    "Go to", ["Upload CSV", "Plot Data", "Generated Plots", "Database Manipulation"]
+)
+if page == "Upload CSV":
+    page1()
+elif page == "Plot Data":
+    page2()
+elif page == "Generated Plots":
+    page3()
+elif page == "Database Manipulation":
+    page4()
